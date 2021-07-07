410c7bb2aada8f820a737bed11ae4ee213aa8900
eji6u94g <seanmybear@gmail.com>
2021-07-07 16:46:42 +0800

    Implement function for generating password.

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const passwordGenerator = require('./password_generator.js')
 const app = express()
 const port = 3000
 
+//set template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+//set static file route
 app.use(express.static('public'))
+
+//set body-parser
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const password = passwordGenerator(req.body)
+  res.render('index', { password })
 })
 
 app.listen(port, () => {
---
+++ password_generator.js
@@ -0,0 +1,52 @@
+function passwordGenerator(options) {
+  //possible characters
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const upperCaseLetters = lowerCaseLetters.toUpperCase()
+  const numbers = '1234567890'
+  const symbols = '`~!@$%^&*()-_+={}[]|;:"<>,.?/'
+
+  //coefficients passed from POST
+  // const options = {
+  //   length: 12,
+  //   lowercase: 'on',
+  //   uppercase: 'on',
+  //   numbers: 'on',
+  //   excludeCharacters: '10Xz'
+  // }
+
+  //Array generated from possible characters
+  let collections = []
+  if (options.lowercase === 'on') {
+    collections = collections.concat(lowerCaseLetters.split(''))
+  }
+  if (options.uppercase === 'on') {
+    collections = collections.concat(upperCaseLetters.split(''))
+  }
+  if (options.numbers === 'on') {
+    collections = collections.concat(numbers.split(''))
+  }
+  if (options.symbols === 'on') {
+    collections = collections.concat(symbols.split(''))
+  }
+
+  //characters which user does not want
+  if (options.excludeCharacters) {
+    collections = collections.filter(character => !options.excludeCharacters.includes(character))
+  }
+
+  //generate password
+  let password = ''
+  for (i = 0; i < options.length; i++) {
+    password += pickCharacters(collections)
+  }
+
+  //return password
+  return password
+}
+
+function pickCharacters(Array) {
+  const collectionsIndex = Math.floor(Math.random() * Array.length)
+  return Array[collectionsIndex]
+}
+
+module.exports = passwordGenerator
